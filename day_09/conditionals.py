c3c2121510033ebe53611ad379f2e061adffdd08
Ameenullah Olalere <bnmustapha1439@gmail.com>
2026-07-07 23:18:30 +0300

    Added some Day_09 solutions.

---
+++ day_09/conditionals.py
@@ -0,0 +1,12 @@
+user = int(input("Please enter your age: "))
+if user >= 18:
+    print("You are old enough to learn to drive.")
+else:
+    print(f"You are too young to learn to drive. You need to wait {18 - user} more years.") 
+
+my_age = 20
+your_age = int(input("Please enter your age: "))
+
+
+
+
