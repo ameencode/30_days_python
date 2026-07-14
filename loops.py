562f2f7058fb988641222eb6511dad13b1a99d72
Ameenullah Olalere <bnmustapha1439@gmail.com>
2026-07-14 17:01:26 +0300

    Added some solutions to Day_10

--- loops.py
+++ loops.py
@@ -1,8 +1,23 @@
 for num in range(0, 11):
     print(num)
+print("Done!")
 
 num = 0
-while num < 10:
+while num <= 10:
     print(num)
     num += 1
-    print(num)
+
+for hash in range(1, 8):
+    print("#" * hash)
+
+sign = 1
+while sign < 9:
+    print("# " * 8)
+    sign += 1
+
+# another way to do the same thing
+for row in range(8):
+    for col in range(8):
+        print("#", end=" ")
+    print()
+
