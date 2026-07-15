9ac2c1a8d3bf7833a796ece1b833c5adf6178d17
Ameenullah Olalere <bnmustapha1439@gmail.com>
2026-07-15 17:25:03 +0300

    Added some solutions to Day_11

---
+++ day_11/functions.py
@@ -0,0 +1,38 @@
+def add_two_numbers(x, y):
+    sum = x + y
+    return sum
+print(add_two_numbers(5, 8))
+
+def area_of_circle(r):
+    pi = 3.14
+    area = pi * r**2
+    return area
+print(area_of_circle(3))
+
+def add_all_nums(*nums):
+    total = 0
+    for num in nums:
+        if type(num) is not type(int):
+            return f"Enter a valid number. All arguments must be a number" 
+        total += num
+    return total
+print(add_all_nums(3, 4, 6, 5, 7))
+
+def convert_celsius_to_fahrenheit(celsius):
+    far = (celsius * 9/5) + 32
+    return far
+print(convert_celsius_to_fahrenheit(50))
+
+def check_season(month):
+    if month in ["december", "january", "februrary"]:
+        return 'Winter'
+    if month in ["march", "april", "may"]:
+        return 'Spring'
+    if month in ["june", "july", "august"]:
+        return 'Summer'
+    if month in ["september", "october", "november"]:
+        return 'Autumn'
+    else:
+        return 'Invalid Month'
+print(check_season(input('Input the name of a month')))
+
